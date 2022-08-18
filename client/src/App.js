f17dedfc41f05f92d15ca47f6b9b63c93fe92c0a
Irene Njeru <irenenjeru8@gmail.com>
2022-08-18 17:52:11 +0300

    add signup.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
+import Signup from './components/Signup';
 import Testing from './components/Testing';
 
 function App() {
   return (
     <div className="App">
-      <Testing/>
+      {/* <Testing/> */}
+      <Signup/>
     </div>
   );
 }
---
+++ client/src/components/Signup.js
@@ -0,0 +1,96 @@
+import React, { useRef, useState, useContext } from "react";
+import { Form, Button, Card, Alert, Container } from "react-bootstrap";
+
+import { Link, useHistory } from "react-router-dom"
+
+// import { OurAuthContext } from "../contexts/OurAuthContext";
+
+
+export default function Signup() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const history = useHistory()
+//   const { signup, setCurrentUser, setMemberId } = useContext(OurAuthContext);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match");
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+
+      console.log("perform signup");
+    //   const res = await signup(emailRef.current.value, passwordRef.current.value);
+
+    //   if (res.id){
+    //     setCurrentUser(res.member_email);
+    //     setMemberId(res.id);
+    //     localStorage.setItem("savedUser", res.member_email);
+    //     history.push("/");
+    //   }
+    //   else if (res.error){
+    //     setError(res.error);
+    //   }
+      // history.push("/")
+    } catch {
+      setError("Failed to create an account");
+    }
+
+    setLoading(false);
+  }
+
+  return (
+    <>
+      <Container
+        className="d-flex justify-content-center mt-4"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "500px" }}>
+          <Card>
+            <Card.Body className="card-form">
+              <h2 className="text-center mb-4">Sign Up</h2>
+              {error && <Alert variant="danger">{error}</Alert>}
+              <Form onSubmit={handleSubmit}>
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" ref={emailRef} required />
+                </Form.Group>
+                <Form.Group id="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" ref={passwordRef} required />
+                </Form.Group>
+                <Form.Group id="password-confirm">
+                  <Form.Label>Password Confirmation</Form.Label>
+                  <Form.Control
+                    type="password"
+                    ref={passwordConfirmRef}
+                    required
+                  />
+                </Form.Group>
+                <Button
+                  variant="secondary"
+                  disabled={loading}
+                  className="w-100 my-4"
+                  type="submit"
+                >
+                  Sign Up
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+          <div className="w-100 text-center mt-2">
+            Already have an account? <Link to="/login">Log In</Link>
+            {/* Already have an account? Log In */}
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+}
