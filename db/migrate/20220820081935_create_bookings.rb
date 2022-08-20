75e952e1ff718c3b9f48c2c444e9e41eba30b331
NDICHU <ndichumuriithi@gmail.com>
2022-08-20 11:30:46 +0300

    Created bookings resources

---
+++ db/migrate/20220820081935_create_bookings.rb
@@ -0,0 +1,15 @@
+class CreateBookings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookings do |t|
+      t.datetime :date
+      t.string :origin
+      t.string :destination
+      t.integer :distance
+      t.integer :cost
+      t.references :user_id, null: false, foreign_key: true
+      t.references :mover_id, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
