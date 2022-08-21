33637cac11402638ac08e46f89a312e3f4ea840d
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-08-21 10:26:49 +0300

    Modify admin controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,4 +2,9 @@ class TestsController < ApplicationController
     def index 
         render json: {text: "Hello from rails api"}
     end
+
+    def show 
+        user =  User.find_by(id: params[:id])
+        render json: user
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
 
-    resources :admins, only: [:show]
+    resources :admins, only: [:show, :index]
     resources :users, only: [:index, :create, :show]
   end
 
-  resources :tests, only: [:index]
+  resources :tests, only: [:index, :show]
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
