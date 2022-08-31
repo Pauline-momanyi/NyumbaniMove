b12ddb3bf6ca2040750e6b55cb3b308700ba8f9d
NDICHU <ndichumuriithi@gmail.com>
2022-08-31 12:00:33 +0300

    Seed user

--- .history/db/seeds_20220831111727.rb
+++ .history/db/seeds_20220831111727.rb
@@ -11,6 +11,5 @@ Admin.create!(name: 'Nyumbani Move', email: 'nyumbanimovers@info.com', phone: '0
 Mover.create!(name: 'Ofisi Move', email: 'ofisimove@info.com', phone: '0702000000', password: '1234', is_admin: false, is_client: false, is_mover: true)
 
 Mover.create!(name: 'Movers & Shakers', email: 'shakers@info.com', phone: '0703000000', password: '1234', is_admin: false, is_client: false, is_mover: true)
-User.create!(name: 'test', email: 'test@gmail.com', phone: '0708000000', password: '1234', is_admin: false, is_client: true, is_mover: false)
 
 puts 'done seeding'
