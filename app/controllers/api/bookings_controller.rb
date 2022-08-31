f91ff4b282744578f2b2715d0a24d59f7aaadcf9
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-09-01 02:02:48 +0300

    update swagger host to remote server

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,5 +1,5 @@
 class Api::BookingsController < ApplicationController
-    # before_action :authorize
+    before_action :authorize
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
